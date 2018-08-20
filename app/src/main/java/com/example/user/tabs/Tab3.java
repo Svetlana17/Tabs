e236706fdbb2ee5191e853018d987d11251c4365
Users <lana.chebotareva.95@mail.ru>
2018-08-20 09:36:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/tabs/Tab3.java
@@ -0,0 +1,19 @@
+package com.example.user.tabs;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_3, container, false);
+
+    }
+}
